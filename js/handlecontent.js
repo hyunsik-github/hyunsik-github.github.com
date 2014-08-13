0cad0620d6a24aee2d8291b0bf1b1c7baa72c909
hyunsik-github <real_hiro@naver.com>
2014-08-14 00:45:16 +0900

    test

--- js/handlecontent.js
+++ js/handlecontent.js
@@ -1,77 +1,95 @@
-﻿	var WHERE="where";
-	var WHAT="what";
-	var WHOG="whog";
-	var WHOM="whom";
-	
-	var restaurants;
-	var menus;
-	var groups;
-	var members;	
-		
-	var selectedMenu;
-	
-	var data;
-	var flag = {
-		"where" : "",
-		"what" : "",
-		"who" : "",
-		"member" : ""
-	};
-		
-	function showContent() {
-		data = {};
-		var dWhen = document.getElementById('div_when');
-		dWhen.innerHTML = "<label onclick='showDataInput(this)'>" + LABEL_WHEN + ": <input type='date' id='date' /></label>";      
-		document.getElementById('date').valueAsDate = new Date();
-      
-		//var dWhere = document.getElementById('div_where');
-		//dWhere.innerHTML += "<label onclick='showDataInput(this)'>" + LABEL_WHERE + ": </label>"; 
-		
-		showRestaurant();
-	}
-    
-    function showRestaurant() {
-    	var dWhere = document.getElementById('div_where');
-		dWhere.innerHTML += "<label onclick='showDataInput(this, showMenu())'>" + LABEL_WHERE + ": </label>"; 
+var WHAT="what";
+var WHOG="whog";
+var WHOM="whom";
 
-    	if(restaurants == null) {
-			showDataInput(dWhere, showMenu);
-		} else {
-			for(var index in restaurants) {
-				dWhere.innerHTML += "<input type='button' value='" + restaurants[index] + "' id='" + restaurants[index] + "' onclick='showMenu(this.value)'>"; 
-			}			
-		}		
-		document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHERE;
+var restaurants;
+var menus;
+var groups;
+var members;	
+
+var selectedMenu;
+
+var data;
+var flag = {
+    "where" : "",
+    "what" : "",
+    "who" : "",
+    "member" : ""
+};
+
+var WHERE = {
+    key : "where",
+    nextFunction : function (value) {			
+
+        var dWhat = document.getElementById('div_what');		
+        dWhat.innerHTML = "<label onclick='makeNewText(this.parentElement)'>" + LABEL_WHAT + ": </label>";
+
+        menus = APPDATA[value];
+        if(menus == 'undefined' || menus == null) {
+            makeNewText(dWhat);
+        } else {			
+            for(var key in menus) {
+                dWhat.innerHTML += "<input type='button' name='div_what_button' value='" + key + " (" + menus[key] + "원)' onclick='setMenu(\"" + key + "\")'>"; 
+            }
+        }
+
+        document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHAT;
+    }
+};
+
+function showContent() {
+    data = {};
+
+    var dWhen = document.getElementById('div_when');
+    dWhen.className = a.className;
+    dWhen.innerHTML = "<label onclick='showDataInput(this)'>" + LABEL_WHEN + ": <input type='date' id='date' /></label>";      
+    document.getElementById('date').valueAsDate = new Date();
+
+    showRestaurant();
+}
+
+function showRestaurant() {
+    var dWhere = document.getElementById('div_where');
+    dWhere.innerHTML += "<label onclick='makeNewText(this.parentElement)'>" + LABEL_WHERE + ": </label>"; 
+
+    if(restaurants == 'undefined' || restaurants == null) {
+        makeNewText(dWhere);
+    } else {
+        for(var index in restaurants) {
+            dWhere.innerHTML += "<input type='button' name='div_where_button' value='" + restaurants[index] + "' onclick='showMenu(this.value)'>"; 
+        }			
+    }		
+    document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHERE;
+}
+
+function showMenu(restaurant) {
+    //setToggle(restaurant);
+
+    var dWhat = document.getElementById('div_what');		
+    dWhat.innerHTML = "<label onclick='makeNewText(this.parentElement)'>" + LABEL_WHAT + ": </label>";
+
+    menus = APPDATA[restaurant];
+    if(menus == 'undefined' || menus == null) {
+        makeNewText(dWhat);
+    } else {			
+        for(var key in menus) {
+            dWhat.innerHTML += "<input type='button' name='div_what_button' value='" + key + " (" + menus[key] + "원)' onclick='setMenu(\"" + key + "\")'>"; 
+        }
     }
 
-    function showMenu(restaurant) {
-		//setToggle(restaurant);
+    document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHAT;
+}
 
-		var dWhat = document.getElementById('div_what');		
-		dWhat.innerHTML = "<label onclick='showDataInput(this, showGroup)'>" + LABEL_WHAT + ": </label>";
-
-		menus = APPDATA[restaurant];
-		if(menus == null || menus == undefined) {
-			showDataInput(dWhat, showMenu);
-		} else {			
-			for(var key in menus) {
-				dWhat.innerHTML += "<input type='button' value='" + key + " (" + menus[key] + "원)' id='" + restaurant + "_" + key + "' onclick='setMenu(\"" + key + "\")'>"; 
-			}
-		}
-		
-		document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHAT;
-	}
-	
-	/*function setToggle(restaurant) {
-		if(data[WHERE] == restaurant) {
+/*function setToggle(restaurant) {
+		if(data[WHERE[key]] == restaurant) {
 			console.log(restaurant + " is OFF");
-			data[WHERE] = null;
+			data[WHERE[key]] = null;
 			for(var index in restaurants) {
 				document.getElementById(restaurants[index]).readOnly = false;
 			}
 		}
 		else {
-			data[WHERE] = restaurant;
+			data[WHERE[key]] = restaurant;
 			for(var index in restaurants) {
 				if(restaurant[index] != restaurant) {
 					document.getElementById(restaurants[index]).readOnly = true;
@@ -81,118 +99,144 @@
 			}
 		}
 	}*/
-  
-	function setMenu(menu) {
-		selectedMenu = menu;
-		if(!(data.hasOwnProperty(menu))){
-			data[menu]=new Array();
-		}
-		showGroup();    
-	}
 
-    function showGroup() {
-		var dGroup = document.getElementById('div_whog'); 
-		dGroup.innerHTML = "<label onclick='showDataInput(this, showMember)'>" + LABEL_WHO_GROUP + ":</label>";
-		dGroup.innerHTML += "<input type='button' value='All' id='all' onclick='showMember(this.value)'>";   
-		for(var index in groups) {
-			dGroup.innerHTML += "<input type='button' value='" + groups[index] + "' id='" + groups[index] + "' onclick='showMember(this.value)'>"; 
+function setMenu(menu) {
+    selectedMenu = menu;
+    if(!(data.hasOwnProperty(menu))){
+        data[menu] = [];
+    }
+    showGroup();    
+}
+
+function showGroup() {
+    var dGroup = document.getElementById('div_whog'); 
+    dGroup.innerHTML = "<label onclick='showDataInput(this, showMember)'>" + LABEL_WHO_GROUP + ":</label>";
+    dGroup.innerHTML += "<input type='button' value='All' id='all' onclick='showMember(this.value)'>";   
+    for(var index in groups) {
+        dGroup.innerHTML += "<input type='button' value='" + groups[index] + "' id='" + groups[index] + "' onclick='showMember(this.value)'>"; 
         document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHO_GROUP;
-		}
     }
-    
-    function showMember(group) {
-		var dMember = document.getElementById('div_whom');   
-		dMember.innerHTML = "<label onclick='showDataInput(this)'>" + LABEL_WHO_MEMBER + ":</label>";
-		var memberArray;
-		if("All" == group) {
-			memberArray = members;
-		} else {
-			memberArray = APPDATA[group];
-		}
-    
-		for(var index in memberArray) {
-			dMember.innerHTML += "<input type='button' value='" + memberArray[index] + "' id='" + memberArray[index] + "' onclick='addMemberData(this.value)'>"; 
-		}
-		document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHO_MEMBER;
-    }
-	
-	function addMemberData(member) {
-		var memberArr = data[selectedMenu];
-		var isFind = false;
-		for(var i in memberArr) {
-			if(memberArr[i] == member) {
-				isFind = true;
-				break;
-			}
-		}
-		if(!isFind) {
-			memberArr.push(member);
-		}
-		showResult();
-	}
-	
-	function removeMemberData(member) {
-		var memberArr = data[selectedMenu];
-		var isFind = false;
-		var index;
-		for(var i in memberArr) {
-			if(memberArr[i] == member) {
-				isFind = true;
-				index = i;
-				break;
-			}
-		}
-		if(isFind) {
-			memberArr.splice(index, 1);
-		}
-	}
+}
 
-	function showDataInput(element, func) {
-        var doc = element.parentElement;
-        if(flag[doc.id] == "+") {
-			flag[doc.id] = "-";  
-			var child = document.getElementById('input_'+doc.id);
-			doc.removeChild(child);
-			child = document.getElementById('confirm_'+doc.id);
-			doc.removeChild(child);
-        } else {
-         	flag[doc.id] = "+";  
-			doc.innerHTML += "<input type='text' id='input_" + doc.id + "'>";
-    	    doc.innerHTML += "<input type='button' id='confirm_" + doc.id + "' value='확인' onclick='addData(this.parentElement, " + func + ")'>";
+function showMember(group) {
+    var dMember = document.getElementById('div_whom');   
+    dMember.innerHTML = "<label onclick='showDataInput(this)'>" + LABEL_WHO_MEMBER + ":</label>";
+    var memberArray;
+    if("All" == group) {
+        memberArray = members;
+    } else {
+        memberArray = APPDATA[group];
+    }
+
+    for(var index in memberArray) {
+        dMember.innerHTML += "<input type='button' value='" + memberArray[index] + "' id='" + memberArray[index] + "' onclick='addMemberData(this.value)'>"; 
+    }
+    document.getElementById('div_help_label').innerHTML = MSG_SELECT_WHO_MEMBER;
+}
+
+function addMemberData(member) {
+    var memberArr = data[selectedMenu];
+    var isFind = false;
+    for(var i in memberArr) {
+        if(memberArr[i] == member) {
+            isFind = true;
+            break;
         }
-	}
-	
-	function addData(doc, callback) {
-		var key = doc.className;
-		var d = document.getElementById('input_'+doc.id);
-		var value = document.getElementById('input_'+doc.id).value;
-		APPDATA[key].push(value);
-		//save(APPDATA);
-		callback(value);
-	}
-	
-	function showResult() {
-		var result = document.getElementById('result');   
-		result.innerHTML = "<input type='button' value='save' onclick='save()'>";
-		for(var key in menus) {		
-			if(data.hasOwnProperty(key)){
-				result.innerHTML += "<br>";
-				result.innerHTML += data[WHERE] + " : ";
-				result.innerHTML += "[" + key + " : " + menus[key] + "원] - ";		
-				
-				var memberArr = data[key];
-				
-				for(var i in memberArr) {
-					result.innerHTML += " " + memberArr[i];
-				}
-				result.innerHTML += "</br>";
-			}
-		}
-	}
-	
-	function save(data) {
-		var str = JSON.stringify(data);
-		console.log(str);
-		writeData(null, str);
-		//writeData(null, data);
-	}
+    }
+    if(!isFind) {
+        memberArr.push(member);
+    }
+    showResult();
+}
+
+function removeMemberData(member) {
+    var memberArr = data[selectedMenu];
+    var isFind = false;
+    var index;
+    for(var i in memberArr) {
+        if(memberArr[i] == member) {
+            isFind = true;
+            index = i;
+            break;
+        }
+    }
+    if(isFind) {
+        memberArr.splice(index, 1);
+    }
+}
+function makeNewText(doc) {
+    var name = doc.id + "_text";
+    var button_cnt = document.getElementsByName(doc.id + "_button").length;
+    var text_cnt = document.getElementsByName(name).length;
+    var newText = "<input type='text' name='" + name + "' id='" + name + (text_cnt + 1) + "' onkeypress='keychecker(event, this)'>";
+
+    if(button_cnt == 0 && text_cnt == 0) {
+        doc.innerHTML += newText;
+    } else {			
+        if(flag[doc.id] == "+") {
+            flag[doc.id] = "-";  
+            var child = document.getElementById(name + (text_cnt));
+            doc.removeChild(child);				
+        } else {
+            flag[doc.id] = "+";  
+            doc.innerHTML += newText;
+        }
+    }
+}
+
+function showDataInput(element) {
+    var doc = element.parentElement;
+    if(flag[doc.id] == "+") {
+        flag[doc.id] = "-";  
+        var child = document.getElementById('input_'+doc.id);
+        doc.removeChild(child);
+        child = document.getElementById('confirm_'+doc.id);
+        doc.removeChild(child);
+    } else {
+        flag[doc.id] = "+";  
+        doc.innerHTML += "<input type='text' id='input_" + doc.id + "' onkeypress='keychecker(event)'>";
+        //doc.innerHTML += "<input type='button' id='confirm_" + doc.id + "' value='확인' onclick='addData(this.parentElement)'>";
+    }
+}
+
+function addData(doc) {
+    var key = doc.className;
+    var d = document.getElementById('input_'+doc.id);
+    var value = document.getElementById('input_'+doc.id).value;
+    APPDATA[key].push(value);
+    //save(APPDATA);
+    callback(value);
+}
+
+function showResult() {
+    var result = document.getElementById('result');   
+    result.innerHTML = "<input type='button' value='save' onclick='save()'>";
+    for(var key in menus) {		
+        if(data.hasOwnProperty(key)){
+            result.innerHTML += "<br>";
+            result.innerHTML += data[WHERE[key]] + " : ";
+            result.innerHTML += "[" + key + " : " + menus[key] + "원] - ";		
+
+            var memberArr = data[key];
+
+            for(var i in memberArr) {
+                result.innerHTML += " " + memberArr[i];
+            }
+            result.innerHTML += "</br>";
+        }
+    }
+}
+
+function save(data) {
+    var str = JSON.stringify(data);
+    console.log(str);
+    writeData(null, str);
+    //writeData(null, data);
+}
+
+function keychecker(event, element) {
+    var doc = element.parentElement;		
+    if(event.keyCode == 13) {
+        console.log(element.value);		
+    }
+}
--- js/handlegapi.js
+++ js/handlegapi.js
@@ -1,197 +1,197 @@
-﻿	var CLIENT_ID="1003542821097-ch60bub61oa3gq84p7gv3d90std7bq1j.apps.googleusercontent.com";
-	var SCOPES=[
-		'https://www.googleapis.com/auth/drive',
-		'https://www.googleapis.com/auth/drive.file',
-		'https://www.googleapis.com/auth/drive.appdata'
-	];
-	
-	var APPDATA_NAME = 'data.json';
-	var APPDATA_META;
-	var APPDATA;
-	
-	function handleClientLoad() {
-		checkAuth();
-    }
-    
-    function checkAuth() {
-		gapi.auth.authorize(
-			{'client_id': CLIENT_ID, 'scope' : SCOPES.join(' '), 'immediate': false}, handleAuthResult
-		);
-    }
-	
-	function executeWithDrive(callback) {
-		gapi.client.load('drive', 'v2', callback);
-    }
-	
-	function handleAuthResult(authResult) {
-		if (authResult) {
-			// Access token has been successfully retrieved, requests can be sent to the API
-			executeWithDrive(getAppData);
-			//listFilesInApplicationDataFolder(checkApplicationDataFile);           
-			//createApplicationDataFile();
-		} else {
-			// No access token could be retrieved, force the authorization flow.
-			gapi.auth.authorize(
-				{'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false}, handleAuthResult
-			);
-		}
-    }
-	
-	function getAppData() {
-		var request = gapi.client.drive.files.get({
-			'fileId': 'appdata'
-		});
-		request.execute(function(resp) {
-			APPDATA_META = resp;
-			console.log(resp.title + ':' + APPDATA_META.id);
-			getDataFromAppData();
-		});
-    }
-	
-	function getDataFromAppData() {     
-		var retrievePageOfFiles = function(request, result) {
-			request.execute(function(resp) {
-				result = result.concat(resp.items);
-				var nextPageToken = resp.nextPageToken;
-				if (nextPageToken) {
-					request = gapi.client.drive.files.list({
-						'pageToken': nextPageToken
-					});
-					retrievePageOfFiles(request, result);
-				} else {
-					checkData(result);
-				}
-			});
-		}
-		//var query = '\'appdata\' in parents and title = \'' + APPDATA_NAME + '\'';
-		var query = 'title = \'aaa.aaa\'';
-		var listRequest = gapi.client.drive.files.list({
-			'q': query        
-		});
-		retrievePageOfFiles(listRequest, []);
-    }
-	
-	function checkData(result) {
-		var isFind = false;
-		var item;
-		for(var index in result) {
-			item = result[index];
-			if(typeof (item) != 'undefined') {
-			  //if(item.title == APPDATA_NAME) {
-				console.log('Data Exist');
-				isFind = true;
-				break;
-			  //}
-			}
-		}     
-      
-		if(isFind) {        
-			getFileContent(item, function (response) {
-				APPDATA = JSON.parse(response);
-				for(var key in APPDATA) {   
-					if(WHOG == key) {
-						groups = APPDATA[key];
-					} else if(WHOM == key) {
-						members = APPDATA[key];
-					} else if(WHERE == key) {
-						restaurants = APPDATA[key];
-					}
-					//console.log(key + ":" + APPDATA[key]);
-				}
-				showContent();
-			});    
-		} else {
-			console.log('No Data File');
-			var createData = function () {
-				var metadata = {
-					'title': 'data.json',
-					'mimeType': 'application/json',
-					'parents': [{
-					'id': APPDATA_META.id
-					}]
-				};
+var CLIENT_ID="1003542821097-ch60bub61oa3gq84p7gv3d90std7bq1j.apps.googleusercontent.com";
+var SCOPES=[
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive.appdata'
+];
 
-				var request = gapi.client.request({
-					'path': '/drive/v2/files',
-					'method': 'POST',
-					'params': {'uploadType': 'multipart'},
-					'body': JSON.stringify(metadata)
-				});
-			  
-				request.execute(function(file) {
-					console.log('Data Created');
-					console.log(file);
-				});
-			}
+var APPDATA_NAME = 'data.json';
+var APPDATA_META;
+var APPDATA;
+
+function handleClientLoad() {
+    checkAuth();
+}
+
+function checkAuth() {
+    gapi.auth.authorize(
+        {'client_id': CLIENT_ID, 'scope' : SCOPES.join(' '), 'immediate': false}, handleAuthResult
+    );
+}
+
+function executeWithDrive(callback) {
+    gapi.client.load('drive', 'v2', callback);
+}
+
+function handleAuthResult(authResult) {
+    if (authResult) {
+        // Access token has been successfully retrieved, requests can be sent to the API
+        executeWithDrive(getAppData);
+        //listFilesInApplicationDataFolder(checkApplicationDataFile);           
+        //createApplicationDataFile();
+    } else {
+        // No access token could be retrieved, force the authorization flow.
+        gapi.auth.authorize(
+            {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false}, handleAuthResult
+        );
+    }
+}
+
+function getAppData() {
+    var request = gapi.client.drive.files.get({
+        'fileId': 'appdata'
+    });
+    request.execute(function(resp) {
+        APPDATA_META = resp;
+        console.log(resp.title + ':' + APPDATA_META.id);
+        getDataFromAppData();
+    });
+}
+
+function getDataFromAppData() {     
+    var retrievePageOfFiles = function(request, result) {
+        request.execute(function(resp) {
+            result = result.concat(resp.items);
+            var nextPageToken = resp.nextPageToken;
+            if (nextPageToken) {
+                request = gapi.client.drive.files.list({
+                    'pageToken': nextPageToken
+                });
+                retrievePageOfFiles(request, result);
+            } else {
+                checkData(result);
+            }
+        });
+    };
+    //var query = '\'appdata\' in parents and title = \'' + APPDATA_NAME + '\'';
+    var query = 'title = \'aaa.aaa\'';
+    var listRequest = gapi.client.drive.files.list({
+        'q': query        
+    });
+    retrievePageOfFiles(listRequest, []);
+}
+
+function checkData(result) {
+    var isFind = false;
+    var item;
+    for(var index in result) {
+        item = result[index];
+        if(typeof (item) != 'undefined') {
+            //if(item.title == APPDATA_NAME) {
+            console.log('Data Exist');
+            isFind = true;
+            break;
+            //}
+        }
+    }     
+
+    if(isFind) {        
+        getFileContent(item, function (response) {
+            APPDATA = JSON.parse(response);
+            for(var key in APPDATA) {   
+                if(WHOG == key) {
+                    groups = APPDATA[key];
+                } else if(WHOM == key) {
+                    members = APPDATA[key];
+                } else if(WHERE[key] == key) {
+                    restaurants = APPDATA[key];
+                }
+                //console.log(key + ":" + APPDATA[key]);
+            }
+            showContent();
+        });    
+    } else {
+        console.log('No Data File');
+        var createData = function () {
+            var metadata = {
+                'title': 'data.json',
+                'mimeType': 'application/json',
+                'parents': [{
+                    'id': APPDATA_META.id
+                }]
+            };
+
+            var request = gapi.client.request({
+                'path': '/drive/v2/files',
+                'method': 'POST',
+                'params': {'uploadType': 'multipart'},
+                'body': JSON.stringify(metadata)
+            });
+
+            request.execute(function(file) {
+                console.log('Data Created');
+                console.log(file);
+            });
+        };
         createData();
-		}
     }
-	
-	function getFileContent(file, callback) {
-		var url;
-		if (file.downloadUrl) {
-			url = file.downloadUrl;
-		} else if (file.exportLinks){
-			url = file['exportLinks']['text/plain'];
-		}
-		//TODO:temp
-		url = 'https://docs.google.com/feeds/download/documents/export/Export?id=1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs&exportFormat=txt';
-		var accessToken = gapi.auth.getToken().access_token;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
-		xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-		xhr.onload = function() {
-			callback(xhr.responseText);
-		};
-		 xhr.onerror = function() {
-		};
-		xhr.send();
-    }
-	
-	function writeData(fileId, data) {  
-		//TODO: remove
-		fileId = '1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs';
-		
-		var request = gapi.client.drive.files.get({'fileId': '1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs'});
-		request.execute(function(resp) {
-			updateFile('1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs', resp, data, null);
-		});
-    }
-	
-	function updateFile(fileId, fileMetadata, data,  callback) {
-		const boundary = '-------314159265358979323846';
-		const delimiter = "\r\n--" + boundary + "\r\n";
-		const close_delim = "\r\n--" + boundary + "--";		
+}
 
-		var contentType = 'application/octet-stream';
-			
-		base64Data = Base64.encode(data);
-		
-			
-		var multipartRequestBody =
-			delimiter +
-			'Content-Type: application/json; charest=utf-8\r\n\r\n' +
-			JSON.stringify(fileMetadata) +
-			delimiter +
-			//'Content-Type: ' + contentType + '; charest=utf-8\r\n' +
-			'Content-Transfer-Encoding: base64\r\n' +
-			'\r\n' +
-			base64Data +
-			close_delim;
-
-		var request = gapi.client.request({
-			'path': '/upload/drive/v2/files/' + fileId,
-			'method': 'PUT',
-			'params': {'uploadType': 'multipart', 'alt': 'json'},
-			'headers': {
-				'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-			},
-			'body': multipartRequestBody});
-			
-		if (!callback) {
-			callback = function(file) {
-				console.log(file)
-			};
-		}
-		request.execute(callback);
+function getFileContent(file, callback) {
+    var url;
+    if (file.downloadUrl) {
+        url = file.downloadUrl;
+    } else if (file.exportLinks){
+        url = file['exportLinks']['text/plain'];
     }
+    //TODO:temp
+    url = 'https://docs.google.com/feeds/download/documents/export/Export?id=1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs&exportFormat=txt';
+    var accessToken = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.onload = function() {
+        callback(xhr.responseText);
+    };
+    xhr.onerror = function() {
+    };
+    xhr.send();
+}
+
+function writeData(fileId, data) {  
+    //TODO: remove
+    fileId = '1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs';
+
+    var request = gapi.client.drive.files.get({'fileId': '1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs'});
+    request.execute(function(resp) {
+        updateFile('1q9-dUZ21i5xPLLBOHxPoteKhdpdtKEJKz6dxIF9peBs', resp, data, null);
+    });
+}
+
+function updateFile(fileId, fileMetadata, data,  callback) {
+    const boundary = '-------314159265358979323846';
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const close_delim = "\r\n--" + boundary + "--";		
+
+    var contentType = 'application/octet-stream';
+
+    base64Data = Base64.encode(data);
+
+
+    var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json; charest=utf-8\r\n\r\n' +
+        JSON.stringify(fileMetadata) +
+        delimiter +
+        //'Content-Type: ' + contentType + '; charest=utf-8\r\n' +
+        'Content-Transfer-Encoding: base64\r\n' +
+        '\r\n' +
+        base64Data +
+        close_delim;
+
+    var request = gapi.client.request({
+        'path': '/upload/drive/v2/files/' + fileId,
+        'method': 'PUT',
+        'params': {'uploadType': 'multipart', 'alt': 'json'},
+        'headers': {
+            'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody});
+
+    if (!callback) {
+        callback = function(file) {
+            console.log(file);
+        };
+    }
+    request.execute(callback);
+}
